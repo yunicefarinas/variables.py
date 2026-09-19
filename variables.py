56750d92c0dc38183d99fd99cd21f7a3961570e2
Yunice <farinasyuniceangel@gmail.com>
2026-09-19 08:13:46 -0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,75 @@
+# Variable Fun
+
+# Multiplication
+print("Please enter the first number")
+num1 = int(input())
+
+print("Please enter the second number")
+num2 = int(input())
+
+answer1 = num1 * num2
+
+print("The answer to your query is", answer1)
+
+
+# Division
+print("Please enter the first number")
+num3 = int(input())
+
+print("Please enter the second number")
+num4 = int(input())
+
+answer2 = num3 / num4
+
+print("The answer to your query is", answer2)
+
+
+# Division without remainder
+print("Please enter the first number")
+num5 = int(input())
+
+print("Please enter the second number")
+num6 = int(input())
+
+answer3 = num5 // num6
+
+print("The answer to your query is", answer3)
+
+
+# Modulo
+print("Please enter the first number")
+num7 = int(input())
+
+print("Please enter the second number")
+num8 = int(input())
+
+answer4 = num7 % num8
+
+print("The answer to your query is", answer4)
+
+
+# Addition
+print("Please enter the first number")
+num9 = int(input())
+
+print("Please enter the second number")
+num10 = int(input())
+
+answer5 = num9 + num10
+
+print("The answer to your query is", answer5)
+
+
+# Subtraction
+print("Please enter the first number")
+num11 = int(input())
+
+print("Please enter the second number")
+num12 = int(input())
+
+answer6 = num11 - num12
+
+print("The answer to your query is", answer6)
+
+
+print("Yunice Angel Mae Farinas")
